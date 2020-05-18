5bac0b1b4d0ff5c3af5838adab5cd334dcb83dc4
Everything Smart Home <53482654+EverythingSmartHome@users.noreply.github.com>
2020-05-18 16:11:28 +0100

    Create fingerprint_led.ino

---
+++ fingerprint_led.ino
@@ -0,0 +1,154 @@
+/*************************************************** 
+  This is an example sketch for our optical Fingerprint sensor
+
+  Designed specifically to work with the Adafruit BMP085 Breakout 
+  ----> http://www.adafruit.com/products/751
+
+  These displays use TTL Serial to communicate, 2 pins are required to 
+  interface
+  Adafruit invests time and resources providing this open source code, 
+  please support Adafruit and open-source hardware by purchasing 
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.  
+  BSD license, all text above must be included in any redistribution
+ ****************************************************/
+ /***************************************************
+  This sketch is intended for use with the guide on Everything Smart Home
+  
+  ****************************************************/
+  
+
+
+#include <Adafruit_Fingerprint.h>
+#define greenLED 4    //Output Pin of Green LED
+#define redLED 5      //Output Pin of Red LED
+SoftwareSerial mySerial(2, 3);
+
+Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
+
+void setup()  
+{
+  Serial.begin(9600);
+  while (!Serial);  // For Yun/Leo/Micro/Zero/...
+  delay(100);
+  Serial.println("\n\nAdafruit finger detect test");
+
+  pinMode(redLED, OUTPUT);
+  pinMode(greenLED, OUTPUT);
+
+  // set the data rate for the sensor serial port
+  finger.begin(57600);
+  delay(5);
+  if (finger.verifyPassword()) {
+    Serial.println("Found fingerprint sensor!");
+  } else {
+    Serial.println("Did not find fingerprint sensor :(");
+    while (1) { delay(1); }
+  }
+
+  finger.getTemplateCount();
+
+    if (finger.templateCount == 0) {
+    Serial.print("Sensor doesn't contain any fingerprint data. Please run the 'enroll' example.");
+  } 
+  else {
+    Serial.println("Waiting for valid finger...");
+      Serial.print("Sensor contains "); Serial.print(finger.templateCount); Serial.println(" templates");
+  }
+}
+
+void loop()
+{
+  uint8_t result = getFingerprintID();
+  if(result == FINGERPRINT_OK) {
+    digitalWrite(greenLED, HIGH);       //Fingerprint scanner returned a match, turn on Green LED and turn off Red LED
+    digitalWrite(redLED, LOW);
+    delay(500);
+  } else if (result == FINGERPRINT_NOTFOUND) {
+    digitalWrite(greenLED, LOW);        //No match, turn off Green LED and turn on red LED
+    digitalWrite(redLED, HIGH);
+    delay(500);
+  } else if (result == FINGERPRINT_NOFINGER) {
+    digitalWrite(greenLED, LOW);     //Set both LED's off until a finger is placed on the scanner
+    digitalWrite(redLED, LOW);
+  } else {
+    digitalWrite(greenLED, LOW); //Some other error occured, flash the red LED twice to indicate.
+    digitalWrite(redLED, HIGH);
+    delay(200);
+    digitalWrite(redLED, LOW);
+    delay(200);
+    digitalWrite(redLED, HIGH);
+    delay(200);
+    digitalWrite(redLED, LOW);
+    delay(200);
+  }
+  delay(100);            //don't need to run this at full speed.
+}
+
+uint8_t getFingerprintID() {
+  uint8_t p = finger.getImage();
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Image taken");
+      break;
+    case FINGERPRINT_NOFINGER:
+      Serial.println("No finger detected");
+      return p;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      return p;
+    case FINGERPRINT_IMAGEFAIL:
+      Serial.println("Imaging error");
+      return p;
+    default:
+      Serial.println("Unknown error");
+      return p;
+  }
+
+  // OK success!
+
+  p = finger.image2Tz();
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Image converted");
+      break;
+    case FINGERPRINT_IMAGEMESS:
+      Serial.println("Image too messy");
+      return p;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      return p;
+    case FINGERPRINT_FEATUREFAIL:
+      Serial.println("Could not find fingerprint features");
+      return p;
+    case FINGERPRINT_INVALIDIMAGE:
+      Serial.println("Could not find fingerprint features");
+      return p;
+    default:
+      Serial.println("Unknown error");
+      return p;
+  }
+  
+  // OK converted!
+  p = finger.fingerFastSearch();
+  if (p == FINGERPRINT_OK) {
+    Serial.println("Found a print match!");
+    return p;
+  } else if (p == FINGERPRINT_PACKETRECIEVEERR) {
+    Serial.println("Communication error");
+    return p;
+  } else if (p == FINGERPRINT_NOTFOUND) {
+    Serial.println("Did not find a match");
+    return p;
+  } else {
+    Serial.println("Unknown error");
+    return p;
+  }   
+  
+  // found a match!
+  Serial.print("Found ID #"); Serial.print(finger.fingerID); 
+  Serial.print(" with confidence of "); Serial.println(finger.confidence); 
+
+  return finger.fingerID;
+}
